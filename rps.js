1b26a2ce145da01ec3e2eaa60c616eaec2a05b78
Nick <nicholasgreiner1@gmail.com>
2022-12-02 10:37:33 -0500

    Finished getComputerTurn function

--- rps.js
+++ rps.js
@@ -3,7 +3,16 @@
 // Rock-paper-Scissors only using the console
 
 // get computer turns
-// 1 - rock, 2 - paper, 3 - scissors
+// generates a number between 1 - 3
+// 1 - Rock 2 - Paper 3 - Scissors
 function getComputerTurn() {
-    
+    let num = Math.floor(Math.random() * 3) + 1;
+    if (num == 1) {
+        return 'Rock'
+    } else if (num == 2) {
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
 }
+console.log(getComputerTurn());
