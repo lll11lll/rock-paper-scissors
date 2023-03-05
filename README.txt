e34ef0df56d085f99d0848f2bde43964973b103f
Nick <nicholasgreiner1@gmail.com>
2023-03-04 21:44:05 -0500

    Completed RPS

--- README.txt
+++ README.txt
@@ -1,1 +1,33 @@
 Rock paper scisscors game through the console.
+
+
+computer vs player
+
+two different containers
+health 
+5/5
+under headers called actions
+3 buttons with rock papers and scissors
+
+
+first create a pop up that alerts the user on how to play RPS
+ask the user to choose from the button below
+just try to get it to work through the console
+-- asign each button to send a value 
+Rock = 1 
+paper = 2
+scissors = 3
+based on that we check if the user or computer won
+have this loop 5 times
+if the user won the we display our lives - 1
+if the computer won the we display computer lives - 1
+
+
+
+once confirmed is press then we do the check with the if statments
+get the computer to choose either rock paper or scisscors
+CSS 
+have icons for each actions
+makes the button bigger
+better button design
+overall better design
--- rps.js
+++ rps.js
@@ -5,6 +5,51 @@
 // get computer turns
 // generates a number between 1 - 3
 // 1 - Rock 2 - Paper 3 - Scissors
+
+// When lives are zero make a pop up depending who actually won.
+
+
+const rockButton = document.querySelector('.rockPButton')
+const paperButton = document.querySelector('.paperPButton')
+const scissorButton = document.querySelector('.scissorPButton')
+const confirmButton = document.querySelector('.confirmButton')
+let playerChoice
+let playerLives = 5
+let computerLives = 5
+rockButton.addEventListener('click', function(e) {
+    alert("Player Chooses Rock!")
+    playerChoice = 'rock'
+    rockButton.classList.add('greenBG')
+    paperButton.classList.remove('greenBG')
+    scissorButton.classList.remove('greenBG')
+    console.log(playerChoice)
+
+})
+
+paperButton.addEventListener('click', function(e) {
+    alert("Player Chooses Paper!")
+    playerChoice = 'paper'
+    paperButton.classList.add('greenBG')
+    rockButton.classList.remove('greenBG')
+    scissorButton.classList.remove('greenBG')
+    console.log(playerChoice)
+})
+
+scissorButton.addEventListener('click', function(e) {
+    alert("Player Chooses Scissors!")
+    playerChoice = 'scissors'
+    scissorButton.classList.add('greenBG')
+    paperButton.classList.remove('greenBG')
+    rockButton.classList.remove('greenBG')
+    console.log(playerChoice)
+})
+confirmButton.addEventListener('click', function(e) {
+    let computerTurn = getComputerTurn()
+    alert(playRound(computerTurn, playerChoice))
+    alert(`Player's lives ${playerLives}\t\t|\t\tComputer's lives ${computerLives}`)
+
+})
+
 function getComputerTurn() {
     let num = Math.floor(Math.random() * 3) + 1;
     if (num == 1) {
@@ -16,44 +61,86 @@ function getComputerTurn() {
     }
 }
 
-function getPlayerTurn() {
-    let input = prompt('Rock, Paper, or Scissors').toLowerCase()
-    if (input === 'rock') {
-        return 'rock'
-    } else if (input === 'paper') {
-        return 'paper'
-    } else if (input === 'scissors') {
-        return 'scissors'   
-    } else {
-        console.log('Invalid input')
-    }
-}
-
-
 // Get the inputs of computer and player and then compare 
 function playRound(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) {
         return 'Tie'
     } else if (computerChoice === 'rock' && playerChoice === 'paper') {
+        computerLives -= 1;
+        updateComputerLives(computerLives)
         return `You win! ${playerChoice} beats ${computerChoice}`
     } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+        playerLives -= 1;
+        updatePlayerLives(playerLives)
         return `You Lose! ${computerChoice} beats ${playerChoice}`
     } else if (computerChoice === 'paper' && playerChoice === 'rock') {
+        playerLives -= 1;
+        updatePlayerLives(playerLives)
         return `You Lose! ${computerChoice} beats ${playerChoice}`
     } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+        computerLives -= 1;
+        updateComputerLives(computerLives)
         return `You win! ${playerChoice} beats ${computerChoice}`
     } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+        computerLives -= 1;
+        updateComputerLives(computerLives)
         return `You win! ${playerChoice} beats ${computerChoice}`
     } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+        playerLives -= 1;
+        updatePlayerLives(playerLives)
         return `You Lose! ${computerChoice} beats ${playerChoice}`
     }
 }
 
-function main() {
-    for (let i = 0; i < 5; i++) {
-        let playerTurn = getPlayerTurn()
-        let computerTurn = getComputerTurn()
-        console.log(playRound(computerTurn, playerTurn))
+function updateComputerLives(computerLives) {
+    let heartImages = ['images/heartFindNBG.png', 'images/heartHollow.png']
+    const heartImgs = document.querySelectorAll('.cHeart');
+    for(let i = 0; i < heartImgs.length; i++) {
+        let imgIndex;
+        if (i < computerLives) {
+            imgIndex = 0;
+        } else {
+            imgIndex = 1;
+        }
+        heartImgs[i].src = heartImages[imgIndex];
     }
 }
-main()
+
+function updatePlayerLives(playerLives) {
+    let heartImages = ['images/heartFindNBG.png', 'images/heartHollow.png']
+    const heartImgs = document.querySelectorAll('.pHeart');
+    for(let i = 0; i < heartImgs.length; i++) {
+        let imgIndex;
+        if (i < playerLives) {
+            imgIndex = 0;
+        } else {
+            imgIndex = 1;
+        }
+        heartImgs[i].src = heartImages[imgIndex];
+    }
+}
+// function main() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerTurn = getPlayerTurn()
+//         let computerTurn = getComputerTurn()
+//         console.log(playRound(computerTurn, playerTurn))
+//     }
+// }
+// main()
+
+
+
+
+
+// function getPlayerTurn() {
+//     let input = prompt('Rock, Paper, or Scissors').toLowerCase()
+//     if (input === 'rock') {
+//         return 'rock'
+//     } else if (input === 'paper') {
+//         return 'paper'
+//     } else if (input === 'scissors') {
+//         return 'scissors'   
+//     } else {
+//         console.log('Invalid input')
+//     }
+// }
